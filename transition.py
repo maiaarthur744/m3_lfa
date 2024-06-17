7aa051a53f5b7395326498a83f4ed3d824253604
prrThr <arthur.op9@gmail.com>
2024-06-17 20:35:43 -0300

    +2 examples

--- transition.py
+++
@@ -1,7 +0,0 @@
-class Transition: # classe que vamos usar para armazenar as informações lidas no txt
-    def __init__(self, currentState, currentSymbol, newState, newSymbol, direction):
-        self.currentState = ""
-        self.currentSymbol = ""
-        self.newState = ""
-        self.newSymbol = ""
-        self.direction = ""
